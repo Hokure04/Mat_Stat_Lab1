d7413c5e4957a9c9e0fc1a704c2b2acbe3e1da2c
Hokure04 <hokure.04@gmail.com>
2024-03-19 22:22:40 +0300

    average

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import csv
+import math
 
 with open('mobile_phones.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    count = 0
+
+    for rows in reader:
+        count += 1
+    print(f'Всего строк {count}')
 
     csvfile.seek(0)
     three_g_rows = []
@@ -43,23 +49,39 @@ with open('mobile_phones.csv', newline='') as csvfile:
         if row['n_cores'] == str(max_core_n):
             n_cores_rows.append(row)
 
-    for row in n_cores_rows:
-        print(row)
-
-
-
-
-def sample_average(file, string):
-    count = 0
-    for row in reader:
-        count += 1
-
     csvfile.seek(0)
+
     battery_power_sum = 0
+    battery_power_rows = []
     iter = 0
-    for row in reader:
+    for rows in reader:
         if iter == 0:
             iter += 1
             continue
-        battery_power_sum += int(row[string])
+        battery_power_sum += int(rows['battery_power'])
+        battery_power_rows.append(int(rows['battery_power']))
+    sample_average = battery_power_sum/count
+    print(f'Выборочное среднее: {sample_average}')
+
+    # print(sample_average)
+    # print(battery_power_rows)
+
+    m_x = 0
+    for x in  battery_power_rows:
+        m_x += (x - sample_average)**2
+    s_x = m_x/(count-1)
+    print(f'Выборочная дисперсия: {s_x}')
+    sko = math.sqrt(s_x)
+    print(f'Среднее квадратичное отклонение: {sko}')
+
+    median = (count+1)/2
+    print(f'Выбораяная медиана: {median}')
+
+
+
+
+
+    # for row in n_cores_rows:
+    #     print(row)
+
 
